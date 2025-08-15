5e7ae9d79890509415c7cde44e8d9a79bcc7073e
Slim Kammoun <slim.kammoun94@gmail.com>
2025-08-15 11:50:59 +0200

    first kafka commit

---
+++ confluent/intro/producer.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import time
+from random import choice
+from confluent_kafka import Producer
+
+if __name__ == '__main__':
+
+    config = {
+        # User-specific properties that you must set
+        'bootstrap.servers': 'localhost:36077',
+
+        # Fixed properties
+        'acks': 'all'
+    }
+
+    # Create Producer instance
+    producer = Producer(config)
+
+    # Optional per-message delivery callback (triggered by poll() or flush())
+    # when a message has been successfully delivered or permanently
+    # failed delivery (after retries).
+    def delivery_callback(err, msg):
+        if err:
+            print('ERROR: Message failed delivery: {}'.format(err))
+        else:
+            topic = msg.topic()
+            key = msg.key().decode('utf-8')
+            value = msg.value().decode('utf-8')
+            print(f"Produced event to topic {topic}: key = {key:12} value = {value:12}")
+
+    # Produce data by selecting random values from these lists.
+    topic = "purchases"
+    user_ids = ['eabara', 'jsmith', 'sgarcia', 'jbernard', 'htanaka', 'awalther']
+    products = ['book', 'alarm clock', 't-shirts', 'gift card', 'batteries']
+    sleep_time = [0.5, 1, 3]
+
+    count = 0
+    for _ in range(100):
+        user_id = choice(user_ids)
+        product = choice(products)
+        producer.produce(topic, product, user_id, callback=delivery_callback)
+        count += 1
+        time.sleep(choice(sleep_time))
+
+    # Block until the messages are sent.
+    producer.poll(10000)
+    producer.flush()
